daadd6be8bf3a16a04fcadbcffa00acbf512faae
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-03-08 20:18:21 +0000

    added photo policy file

---
+++ app/policies/photo_policy.rb
@@ -0,0 +1,14 @@
+class PhotoPolicy
+  attr_reader :user, :photo
+
+  def initialize(user, photo)
+    @user = user
+    @photo = photo
+  end
+
+  # Our policy is that a photo should only be seen by the owner of followers
+  # of the owner, unless the owner is not provate in which case anyone can see it
+  def show?
+    user == photo.owner || !photo.owner.private? || photo.owner.followers.include?(user)
+  end
+end
